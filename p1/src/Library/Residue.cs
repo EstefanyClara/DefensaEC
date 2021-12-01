4cfcff87bc01c0c53539334abc17d69efbf84d26
fofito29 <estefanyclara058@hotmail.com>
2021-12-01 19:41:50 -0300

    Cambios menores

--- p1/src/Library/Residue.cs
+++ p1/src/Library/Residue.cs
@@ -6,6 +6,13 @@ namespace Ucu.Poo.Defense
 
         public ResidueType Type { get; set; }
 
+        public bool IsOrganic
+        {
+            get
+            {
+                return this.Type.IsOrganic; 
+            }
+        }
         public Residue(string name, ResidueType type)
         {
             this.Name = name;
