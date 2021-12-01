21946cec8bb8b8966c6172b7d0d2451bdff24515
fofito29 <estefanyclara058@hotmail.com>
2021-12-01 20:24:26 -0300

    Cambios 2

--- p4/src/Library/PromoCode.cs
+++ p4/src/Library/PromoCode.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace Ucu.Poo.Defense
 {
-    public class PromoCode
+    public class PromoCode: IOfferItem
     {
-        private string code {get;set;}
+        //private string code {get;set;}
         private int amount;
 
         public int SubTotal
@@ -15,13 +15,15 @@ namespace Ucu.Poo.Defense
             }
             set
             {
-                this.amount = value;
+                if (value>0)
+                {
+                    throw new ArgumentException(); 
+                }
             }
         }
 
-        public PromoCode(string code, int amount)
+        public PromoCode(int amount)
         {
-            this.code = code;
             this.SubTotal = amount;
         }
     }
